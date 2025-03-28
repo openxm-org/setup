f71db7b54e7a55e56b4f887d5d6235701a6d138f
mitsushi-fujimoto <mitsushi.fujimoto@gmail.com>
2025-03-28 16:26:29 +0900

    Updated asir for iOS/android to the version 20250328.

---
+++ docs/2025/android/install.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+echo "This is Asir installer for Android."
+cd 
+if [ -d OpenXM ]; then
+  echo "'OpenXM' directory already exists. Please rename/remove it and try \"sh install.sh\" again."
+else
+  curl -O https://openxm-org.github.io/setup/2025/android/asir_aarch64.tar.gz
+  echo "Extracting Asir archive ..."
+  tar xf asir_aarch64.tar.gz
+  echo "Done!"
+  if [ ! -f .profile ]; then
+    cp ../usr/etc/profile .profile
+  fi
+  if [ ! "$(cat .profile | grep 'source ./OpenXM/rc/bashrc' )" ]; then
+	    echo "source ./OpenXM/rc/bashrc" >> .profile
+  fi
+  rm asir_aarch64.tar.gz
+  echo "Asir setup has done!"
+fi
---
+++ docs/2025/ios/install.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+echo "This is Asir installer for iOS."
+cd
+if [ -d OpenXM ]; then
+  echo "'OpenXM' directory already exists. Please rename/remove it and try \"sh install.sh\" again."
+else
+  wget https://openxm-org.github.io/setup/2025/ios/asir_ios.tar.gz
+  apk add tar
+  echo "Extracting Asir archive ..."
+  tar xf asir_ios.tar.gz
+  echo "Done!"
+  if [ ! -f .profile ]; then
+    cp /etc/profile .profile
+  fi
+  if [ ! "$(cat .profile | grep 'source ./OpenXM/rc/bashrc' )" ]; then
+    echo "source ./OpenXM/rc/bashrc" >> .profile
+  fi
+  rm asir_ios.tar.gz
+  echo "Installing the ncurses library ..."
+  apk add ncurses-dev
+  echo "Asir setup has done!"
+fi
